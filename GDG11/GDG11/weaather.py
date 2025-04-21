f759d243db28558e310a4218e55ff85bd3bb2175
Amirlan <amirlanabd2@gmail.com>
2025-04-21 19:42:13 +0500

    weather

---
+++ GDG11/GDG11/weaather.py
@@ -0,0 +1,15 @@
+import requests
+API_KEY = 'fe18bb89ada0136c321ed2ba37a38acb'
+city = input("City name: ")
+
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=ru&units=metric"
+
+response = requests.get(url)
+data = response.json()
+if data.get('cod') == 200:
+    temp = data['main']['temp']
+    description = data['weather'][0]['description']
+    print(f"\nTemperature in the city {city}: {temp}°C")
+    print(f"Weather: {description}")
+else:
+    print("Write the existing city bruh")
