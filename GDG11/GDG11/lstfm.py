d60e9fa665c825bf3732f683b0750ec3ffe79703
Amirlan <amirlanabd2@gmail.com>
2025-04-21 19:28:37 +0500

    gm

---
+++ GDG11/GDG11/lstfm.py
@@ -0,0 +1,17 @@
+import requests
+
+API_KEY = '057c40c0a7c06cea6620e992b25d5c02'
+
+artist_name = input("Artist name: ")
+
+url = f'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={artist_name}&api_key={API_KEY}&format=json'
+
+response = requests.get(url)
+data = response.json()
+
+if 'toptracks' in data:
+    print(f"\nArtist's top tracks {artist}:")
+    for track in data['toptracks']['track'][:10]:
+        print(f"- {track['name']}")
+else:
+    print("Error u dumb")
